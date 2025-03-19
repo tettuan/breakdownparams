be554b70bace2e8f798c70da7183172f2bb978b2
tettuan <tettuan@gmail.com>
2025-03-19 23:02:39 +0900

    feat: add mod.ts for package exports

---
+++ src/mod.ts
@@ -0,0 +1,10 @@
+export { ParamsParser } from "./params_parser.ts";
+export type {
+  ParamsResult,
+  NoParamsResult,
+  SingleParamResult,
+  DoubleParamsResult,
+  DemonstrativeType,
+  OptionParams,
+  LayerTypeAliasMap,
+} from "./types.ts"; 
