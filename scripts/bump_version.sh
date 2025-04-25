74ad9440a950f47e79031a201c5c5432619689b6
tettuan <tettuan@gmail.com>
2025-04-26 00:20:33 +0900

    fix: update JSR package name in bump_version.sh

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -24,7 +24,7 @@ done
 
 # Try to get latest version from JSR
 echo "Checking latest version from JSR..."
-latest_jsr_version=$(curl -s https://jsr.io/@tettuan/breakdownconfig/versions | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
+latest_jsr_version=$(curl -s https://jsr.io/@tettuan/breakdownparams/versions | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
 
 if [ -z "$latest_jsr_version" ]; then
     echo "Warning: Could not determine latest version from JSR, using local version"
