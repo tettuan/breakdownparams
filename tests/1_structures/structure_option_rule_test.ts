427204acf116bb4d4b686d1a8b87614a16193759
tettuan <tettuan@gmail.com>
2025-06-07 18:30:11 +0900

    refactor: use default constructor in structure test
    - Replace DEFAULT_OPTION_RULE with default constructor in structure_option_rule_test.ts

--- tests/1_structures/structure_option_rule_test.ts
+++ tests/1_structures/structure_option_rule_test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
-import { DEFAULT_OPTION_RULE, ParamsParser } from '../../src/parser/params_parser.ts';
+import { ParamsParser } from '../../src/parser/params_parser.ts';
 
 Deno.test('test_params_parser_structure', () => {
-  const parser = new ParamsParser(DEFAULT_OPTION_RULE);
+  const parser = new ParamsParser();
   assertEquals(typeof parser.parse, 'function');
   assertEquals(parser instanceof ParamsParser, true);
 });
 
 Deno.test('test_params_parser_default_behavior', () => {
-  const parser = new ParamsParser(DEFAULT_OPTION_RULE);
+  const parser = new ParamsParser();
   const result = parser.parse(['--help']);
   assertEquals(result.type, 'zero');
   assertEquals(Array.isArray(result.params), true);
