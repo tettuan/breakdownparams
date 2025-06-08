7661c2de1174fd1e27ee92cd5e1981f5e16a8546
tettuan <tettuan@gmail.com>
2025-06-08 17:20:06 +0900

    fix: update CustomVariableOption pattern in architecture test to match --uv-config

---
+++ src/option-models/tests/0_architecture_option_models_test.ts
@@ -0,0 +1,63 @@
+import { assert, assertEquals } from "jsr:@std/assert@^0.218.2";
+import { FlagOption } from "../flag_option.ts";
+import { ValueOption } from "../value_option.ts";
+import { CustomVariableOption } from "../custom_variable_option.ts";
+import { OptionType } from "../../types/option_type.ts";
+
+Deno.test('Option Models Architecture', async (t) => {
+  await t.step('should maintain option model interfaces', () => {
+    const flagOption = new FlagOption('--help', ['h'], 'Show help message');
+    const valueOption = new ValueOption(
+      '--input',
+      ['i'],
+      false,
+      'Input file',
+      (_v) => ({ isValid: true, validatedParams: [] }),
+    );
+    const customOption = new CustomVariableOption('--uv-config', 'Configuration', /^--uv-config$/);
+
+    assert(flagOption instanceof FlagOption);
+    assert(valueOption instanceof ValueOption);
+    assert(customOption instanceof CustomVariableOption);
+  });
+
+  await t.step('should maintain consistent type definitions', () => {
+    const flagOption = new FlagOption('--help', ['h'], 'Show help message');
+    const valueOption = new ValueOption(
+      '--input',
+      ['i'],
+      false,
+      'Input file',
+      (_v) => ({ isValid: true, validatedParams: [] }),
+    );
+    const customOption = new CustomVariableOption('--uv-config', 'Configuration', /^--uv-config$/);
+
+    assertEquals(flagOption.type, OptionType.FLAG);
+    assertEquals(valueOption.type, OptionType.VALUE);
+    assertEquals(customOption.type, OptionType.CUSTOM_VARIABLE);
+  });
+
+  await t.step('should maintain consistent validation results', () => {
+    const flagOption = new FlagOption('--help', ['h'], 'Show help message');
+    const valueOption = new ValueOption(
+      '--input',
+      ['i'],
+      false,
+      'Input file',
+      (_v) => ({ isValid: true, validatedParams: [] }),
+    );
+    const customOption = new CustomVariableOption('--uv-config', 'Configuration', /^--uv-config$/);
+
+    const flagResult = flagOption.validate('');
+    const valueResult = valueOption.validate('test.txt');
+    const customResult = customOption.validate('--uv-config=test_config');
+
+    console.log('flagResult:', flagResult);
+    console.log('valueResult:', valueResult);
+    console.log('customResult:', customResult);
+
+    assert(flagResult.isValid);
+    assert(valueResult.isValid);
+    assert(customResult.isValid);
+  });
+}); 
